c2b9c7eda11aa767ea67b09702f0b04a92705f9d
iraklizakradze <iraklizakradze@gmail.com>
2024-01-14 21:18:18 +0400

    JS 1

---
+++ script.js
@@ -0,0 +1,54 @@
+/* ===== HOMEWORK 1 ===== */
+
+
+// NEW IF STATEMENT //
+let element = 100;
+
+let result = element < 50 ? 'less than 50' : element > 20 ? 'more than 20' : 'error';
+
+console.log(result);
+
+
+// OLD IF STATEMENT //
+let score = 100;
+
+if (score < 50) {
+    console.log('less than 50')
+} else if(score > 20){
+    console.log('more than 20')
+} else{
+    console.log('error')
+}
+
+
+/* ===== HOMEWORK 2 ===== */
+
+
+// NEW IF STATEMENT //
+let userName = 'mariami';
+
+let resultCondition = userName = 'mariami' ? 'true' : 'false';
+
+console.log(resultCondition);
+
+
+// OLD IF STATEMENT //
+let name = 'mariami'
+
+if (name = 'mariami'){
+    console.log('true')
+} else{
+    console.log('false')
+}
+
+
+/* ===== HOMEWORK 3 switch ===== */
+let fisrtName = 'mariami'
+
+switch (fisrtName){
+    case 'mariami':
+        console.log('true');
+        break;
+    default:
+        console.log('false');
+}
